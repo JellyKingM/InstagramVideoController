26e50744d69a6bb273b8fdc73603db27e88a28e4
Choi Byeong O <istake99@gmail.com>
2026-04-25 20:53:49 +0900

    Release InstagramVideoController 2.1.8

---
+++ InstagramVideoController 2.1.8/shared.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    const shared = {
+        LINKS: {
+            donate: 'https://ko-fi.com/jelly_king',
+            developer: 'https://linktr.ee/Jelly_King'
+        },
+        IDS: {
+            donationMenu: 'donationLink'
+        }
+    };
+
+    globalThis.IVC_SHARED = shared;
+})();
